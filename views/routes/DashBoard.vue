9e7010b65ff0b814bcafb50d925a0b8f9c06483b
kn1cht <ito.turukasiwa@gmail.com>
2018-05-13 03:56:06 +0900

    small fix

--- views/routes/DashBoard.vue
+++ views/routes/DashBoard.vue
@@ -19,7 +19,6 @@
           </div>
           <ChargerList class="panel-body"></ChargerList>
         </div>
-        <DeviceList class="panel-body"></DeviceList>
       </div>
       <div class="col-md-6">
         <div class="panel panel-info">
