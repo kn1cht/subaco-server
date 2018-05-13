b918d520cd5297531c99bcd8de45a39da1cdf2f1
kn1cht <ito.turukasiwa@gmail.com>
2018-05-14 01:38:24 +0900

    add ActiveCharger component

---
+++ views/components/ActiveCharger.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li>{{ activeCharger.manufacturer || 'Unknown' }}</li>
+    <li>{{ activeCharger.name || 'Unknown' }}</li>
+    <li>{{ activeCharger.residual }}</li>
+    <li>{{ activeCharger.capacity }}</li>
+  </ul>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  data() { return { activeCharger : {} }},
+	async created() {
+    const res = await axios.get('/api/user/activeCharger');
+    this.activeCharger = res.data.charger;
+    console.log(this.activeCharger);
+  }
+}
+</script>
--- views/routes/MayFes.vue
+++ views/routes/MayFes.vue
@@ -3,21 +3,41 @@
     <div>
       <UserName></UserName>
     </div>
+    <div class = "content">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <h4><i class="fa fa-fw fa-mobile" aria-hidden="true"></i>端末情報</h4>
+        </div>
+        <ActiveCharger class="panel-body"></ActiveCharger>
+      </div>
       <div class="panel panel-info">
         <div class="panel-heading">
           <h4><i class="fa fa-fw fa-mobile" aria-hidden="true"></i>端末情報</h4>
         </div>
         <DeviceList class="panel-body"></DeviceList>
       </div>
+
+    </div>
   </section>
 </template>
 
+<style scoped>
+div.content {
+    display: flex;
+    flex: 1 0 auto;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+}
+</style>
+
 <script>
+import ActiveCharger from '../components/ActiveCharger.vue';
 import DeviceList from '../components/DeviceList.vue';
 import UserName from '../components/UserName.vue';
 
 export default {
   components : {
+    ActiveCharger,
     DeviceList,
     UserName
   }
