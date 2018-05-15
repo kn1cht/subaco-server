a9eb27303e1cca71dfdba3669c306e87a989e7b9
kn1cht <ito.turukasiwa@gmail.com>
2018-05-16 03:30:47 +0900

    add graph view

--- views/components/ActiveCharger.vue
+++ views/components/ActiveCharger.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <p>{{ activeCharger.manufacturer || '製造元不明' }}</p>
+    <p><small>{{ activeCharger.manufacturer || '製造元不明' }}</small></p>
     <p>{{ activeCharger.name || '名称不明' }}</p>
-    <p>{{ activeCharger.residual }}</p>
-    <p>{{ activeCharger.capacity }}</p>
+      <ChargerSocGraph :residual="activeCharger.residual" :capacity="activeCharger.capacity" :option="this.socOption"></ChargerSocGraph>
+    <p>
+      <small>推定残量</small><br>
+      {{ activeCharger.residual.toFixed(0) }} / {{ activeCharger.capacity }} mAh
+    </p>
   </div>
 </template>
 
@@ -11,13 +14,26 @@
 p {
   font-size: 30px;
 }
+p small {
+  font-size: 20px;
+}
 </style>
 
 <script>
 const axios = require('axios');
+import ChargerSocGraph from './ChargerSocGraph.vue';
 
 export default {
-  data() { return { activeCharger : {} }},
+  data() { return {
+    activeCharger : {},
+    socOption     : {
+      backgroundBorderWidth : 15,
+      percentageTextSize    : 30,
+      percentageY           : 120
+    }
+  }},
+  components : { ChargerSocGraph },
+  methods    : {},
 	async created() {
     const res = await axios.get('/api/user/activeCharger');
     this.activeCharger = res.data.charger;
