0a29171cc70171bddeb79fe94fe530ecf081d5b6
kn1cht <ito.turukasiwa@gmail.com>
2018-05-16 03:30:23 +0900

    fix ChargerSocGraph to enable option setting from parent componet

--- views/components/ChargerOverView.vue
+++ views/components/ChargerOverView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div :class="colSize" v-for="item in items" :key="item._id">
-      <ChargerSocGraph :soc="soc(item.residual, item.capacity)" :name="item.name"></ChargerSocGraph>
+      <ChargerSocGraph :residual="item.residual" :capacity="item.capacity" :option="{text:item.name}"></ChargerSocGraph>
     </div>
   </div>
 </template>
@@ -18,12 +18,7 @@ export default {
     }
   },
   components : { ChargerSocGraph },
-  methods    : {
-    soc(residual, capacity) {
-      const percentage = residual / capacity * 100;
-      return Math.max( Math.min(percentage, 100), 0).toFixed(2);
-    }
-  },
+  methods    : {},
 	async created() {
 		let res = await axios.get('/api/charger/list');
     this.items = res.data.list;
--- views/components/ChargerSocGraph.vue
+++ views/components/ChargerSocGraph.vue
@@ -16,20 +16,30 @@ import 'jquery-circliful';
 export default {
   data() { return {}},
   props : {
-    name : Number,
-    soc  : Number
+    residual : Number,
+    capacity : Number,
+    option   : {
+      type   : Object,
+      default: {}
+    }
+  },
+  methods : {
+    soc(residual, capacity) {
+      const percentage = residual / capacity * 100;
+      return Math.max( Math.min(percentage, 100), 0).toFixed(2);
+    }
   },
   mounted () {
-    $(this.$el).circliful({
+    const defaultOpt = {
       width                 : 10,
       animationStep         : 5,
       foregroundColor       : '#9f3',
       backgroundBorderWidth : 2,
       backgroundColor       : "#eee",
       fontColor             : '#333',
-      percent               : this.soc,
-      text                  : this.name,
-    });
+      percent               : this.soc(this.residual, this.capacity)
+    };
+    $(this.$el).circliful(Object.assign(defaultOpt, this.option));
   }
 }
 </script>
