3879197207eae2546dd97264ccfcdfda782f7535
kn1cht <ito.turukasiwa@gmail.com>
2018-05-18 04:38:11 +0900

    implement charger/activate API

---
+++ routes/api/charger.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Charger = require('../../models/charger.model');
+const User = require('../../models/user.model');
+
+const router = express.Router();
+
+router.get('/list', async(req, res) => {
+  if(req.isUnauthenticated()) {
+    res.send({ ok : false, error : 'Not Authed' });
+    return;
+  }
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  const list = await Charger.find({}).sort('-last_discharge')
+                            .catch((err) => { console.error(err); });
+  res.send({ ok : true, list });
+});
+
+router.get('/activate', async(req, res) => {
+  if(req.isUnauthenticated()) {
+    res.send({ ok : false, error : 'Not Authed' });
+    return;
+  }
+  if(!req.query.id) {
+    res.send({ ok : false, error : 'Please specify id parameter' });
+    return;
+  }
+  await User.update({
+    _id : req.user._id
+  }, { $set: {
+    active_charger_id : req.query.id
+  }});
+
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send({ ok : true });
+});
+module.exports = router;
